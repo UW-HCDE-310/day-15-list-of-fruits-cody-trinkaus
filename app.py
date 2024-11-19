a3907aec96251699b9742679a3156ef80ad40366
cody-trinkaus <ctrink@uw.com>
2024-11-19 10:41:56 -0800

    ignoring keys.py

--- app.py
+++ app.py
@@ -3,9 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-    fruits = [
+    old_fruits = [
         {"name": "apples", "quantity": 3},
             {"name": "oranges", "quantity": 2},
             {"name": "strawberries", "quantity": 6}
     ]
+    fruits = []
+    for fruit in old_fruits:
+        if fruit["name"][0].lower() == 'o' and fruit["quantity"] < 3:
+            fruits.append(fruit)
+        else:
+            continue
+
+
     return render_template("index.html", fruits=fruits)
